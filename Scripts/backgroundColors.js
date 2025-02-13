25bb0a395adb5d77767e3ab47de9fa01a9c5f13b
MuradNoor <murad151cse48@gmail.com>
2025-02-13 22:28:15 +0600

    colors added to the div for mouse enter

---
+++ Scripts/backgroundColors.js
@@ -0,0 +1,79 @@
+// change div color
+    // triangle div
+    const triangleDiv = document.getElementById("triangle");
+
+    triangleDiv.addEventListener("mouseenter", function() {
+        triangleDiv.style.backgroundColor = "#D7CCC8"; // Change color on hover
+    });
+
+    triangleDiv.addEventListener("mouseleave", function() {
+        triangleDiv.style.backgroundColor = "white"; // Revert color when not hovering
+    });
+
+    // rectangle div
+    const rectangleDiv = document.getElementById("rectangle");
+
+    rectangleDiv.addEventListener("mouseenter", function() {
+        rectangleDiv.style.backgroundColor = "#E0F7FA "; // Change color on hover
+    });
+
+    rectangleDiv.addEventListener("mouseleave", function() {
+        rectangleDiv.style.backgroundColor = "white"; // Revert color when not hovering
+    });
+
+    // parallelogram div
+    const parallelogramDiv = document.getElementById("parallelogram");
+
+    parallelogramDiv.addEventListener("mouseenter", function() {
+        parallelogramDiv.style.backgroundColor = "#F1F8E9 "; // Change color on hover
+    });
+
+    parallelogramDiv.addEventListener("mouseleave", function() {
+        parallelogramDiv.style.backgroundColor = "white"; // Revert color when not hovering
+    });
+
+
+    // rhombus div
+    const rhombusDiv = document.getElementById("rhombus");
+
+    rhombusDiv.addEventListener("mouseenter", function() {
+        rhombusDiv.style.backgroundColor = "#DCEDC8"; // Change color on hover
+    });
+
+    rhombusDiv.addEventListener("mouseleave", function() {
+        rhombusDiv.style.backgroundColor = "white"; // Revert color when not hovering
+    });
+
+    // pentagon div
+    const pentagonDiv = document.getElementById("pentagon");
+
+    pentagonDiv.addEventListener("mouseenter", function() {
+        pentagonDiv.style.backgroundColor = "#F3E5F5"; // Change color on hover
+    });
+
+    pentagonDiv.addEventListener("mouseleave", function() {
+        pentagonDiv.style.backgroundColor = "white"; // Revert color when not hovering
+    });
+
+    // ellipse div
+    const ellipseDiv = document.getElementById("ellipse");
+
+    ellipseDiv.addEventListener("mouseenter", function() {
+        ellipseDiv.style.backgroundColor = "#E8F5E9"; // Change color on hover
+    });
+
+    ellipseDiv.addEventListener("mouseleave", function() {
+        ellipseDiv.style.backgroundColor = "white"; // Revert color when not hovering
+    });
+
+    //calculation area div
+    const calcDiv = document.getElementById("calculationArea");
+
+    calcDiv.addEventListener("mouseenter", function() {
+        calcDiv.style.backgroundColor = "#E8F5E9"; // Change color on hover
+    });
+
+    calcDiv.addEventListener("mouseleave", function() {
+        calcDiv.style.backgroundColor = "#FCE4EC"; // Revert color when not hovering
+    });
+
